04fe214dde1b1a196fcb409fadda52621926319a
Alexmdup <alexmdup@gmail.com>
2023-12-03 12:30:13 -0500

    added json simple

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,7 +1,11 @@
 module com.app {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     opens com.app to javafx.fxml;
     exports com.app;
+
+    opens model to javafx.fxml;
+    exports model;
 }

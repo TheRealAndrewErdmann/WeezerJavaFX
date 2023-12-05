f00a6c7d607aa0e292205cefb73aa00bac55344f
TheRealAndrewErdmann <andrewherdmann@gmail.com>
2023-12-05 09:07:55 -0500

    added .fxml files

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,10 +1,15 @@
 module com.app {
+    //com.app = library 
+
     requires javafx.controls;
     requires javafx.fxml;
     requires json.simple;
 
     opens com.app to javafx.fxml;
     exports com.app;
+
+    opens controllers to javafx.fxml;
+    exports controllers;
 
     opens model to javafx.fxml;
     exports model;

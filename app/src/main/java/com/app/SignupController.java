b7b09ca87ee20a9d19de04d441fe0c44f8d23fed
Alexmdup <alexmdup@gmail.com>
2023-12-08 17:25:30 -0500

    added elemnts of signup page

--- app/src/main/java/com/app/SignupController.java
+++ app/src/main/java/com/app/SignupController.java
@@ -1,5 +1,24 @@
 package com.app;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import model.User;
+
 public class SignupController {
+
+    @FXML private TextField firstname;
+    @FXML private TextField lastname;
+    @FXML private TextField email;
+    @FXML private TextField phonenum;
+    @FXML private TextField username;
+    @FXML private TextField password;
     
+    @FXML
+    private void switchToProjects() throws IOException {
+        User newUser = new User(firstname.getText(), lastname.getText(), username.getText(), password.getText(), email.getText(), phonenum.getText());
+        App.setRoot("projects");
+    }
+
 }

81287ff6aea529dac0c773f35887879c902e5d8b
TheRealAndrewErdmann <andrewherdmann@gmail.com>
2023-12-08 19:07:52 -0500

    re-synced changes

--- app/src/main/java/com/app/HomeController.java
+++ app/src/main/java/com/app/HomeController.java
@@ -1,5 +1,12 @@
-package controllers;
+package com.app;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
 
 public class HomeController {
-    
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("home");
+    }
 }
